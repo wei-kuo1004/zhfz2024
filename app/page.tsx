5f7d07734a6a66d288b8503ad6436fbfed7e6e66
weikuo <wei-kuo@pcbut.com.tw>
2024-06-24 16:32:37 +0800

    (fix) test change title

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          首頁 <br className="hidden sm:inline" />
+          災害防治首頁 <br className="hidden sm:inline" />
           Home
         </h1>
         <p className="text-muted-foreground max-w-[700px] text-lg">
