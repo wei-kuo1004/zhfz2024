7fbc38ad78cf17ecb870af6c6933ea0d708a9524
UT-weikuo <148830801+wei-kuo1004@users.noreply.github.com>
2024-06-24 15:53:39 +0800

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Project1",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/CinemaCafe/next-template",
+    docs: "https://ui.shadcn.com",
+  },
+}
