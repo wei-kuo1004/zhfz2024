d653045e68181297f5a63f0bf09b847bb2de3fdd
weikuo <wei-kuo@pcbut.com.tw>
2024-06-30 20:24:02 +0800

    (feat)fourthPage add loadingDot

--- app/fourthScene/page.tsx
+++ app/fourthScene/page.tsx
@@ -1,21 +1,22 @@
 "use client";
-import { buttonVariants } from '@/components/ui/button';
 import { useCharacter } from '@/store/character-store';
 import Link from 'next/link';
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import LoadingDots from '@/components/loadingDot';
 
 
 const backgrounds: Record<'character1' | 'character2' | 'character3', string> = {
-  character1: 'https://wei-kuo1004.github.io/zhfz2024/images/PRD/C1/C1-F-bg.jpg',
-  character2: 'https://wei-kuo1004.github.io/zhfz2024/images/PRD/C2/C2-F-bg.jpg',
-  character3: 'https://wei-kuo1004.github.io/zhfz2024/images/PRD/C3/C3-F-bg.jpg',
+  character1: 'https://wei-kuo1004.github.io/zhfz2024/images/PRD/C1/C1-F-bg.webp',
+  character2: 'https://wei-kuo1004.github.io/zhfz2024/images/PRD/C2/C2-F-bg.webp',
+  character3: 'https://wei-kuo1004.github.io/zhfz2024/images/PRD/C3/C3-F-bg.webp',
 };
 
 const FourthPage = () => {
   const { selectedCharacter } = useCharacter();
   const [character, setCharacter] = useState<'character1' | 'character2' | 'character3'>('character1');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (selectedCharacter) {
@@ -25,32 +26,32 @@ const FourthPage = () => {
 
   const backgroundUrl = backgrounds[character];
 
+  useEffect(() => {
+    const img = new window.Image();
+    img.src = backgroundUrl;
+    img.onload = () => setIsLoading(false);
+  }, [backgroundUrl]);
+
+  if (isLoading) return <LoadingDots />;
+
   return (
-    <>
-    <Head>
-      {/* 第四頁 */}
-      <link rel="preload" href="https://wei-kuo1004.github.io/zhfz2024/images/PRD/C1/C1-F-bg.jpg" as="image" />
-      <link rel="preload" href="https://wei-kuo1004.github.io/zhfz2024/images/PRD/C2/C2-F-bg.jpg" as="image" />
-      <link rel="preload" href="https://wei-kuo1004.github.io/zhfz2024/images/PRD/C3/C3-F-bg.jpg" as="image" />
-    </Head>
-<div
-  className="container mx-auto flex aspect-[1/1.8] min-h-screen flex-col items-center justify-center bg-p1-bg bg-cover p-6"
-  style={{ backgroundImage: `url(${backgroundUrl})`, backgroundSize: 'cover' }}
->
-  <div className="grow"></div> {/* 占位符，讓按鈕在底部 */}
-  <div className="flex justify-center gap-4 items-end mb-0 pb-4"> {/* 將 mb-0 移除，使用 pb-4 控制距離 */}
-    <Link href="/">
-      <Image
-        src="https://wei-kuo1004.github.io/zhfz2024/images/PRD/back.png"
-        alt="描述文字"
-        width={800}
-        height={600}
-        style={{ objectFit: 'cover', width: '100%', height: 'auto' }}
-      />
-    </Link>
-  </div>
-</div>
-</>
+      <div
+        className="container mx-auto flex aspect-[1/1.8] min-h-screen flex-col items-center justify-center bg-p1-bg bg-cover p-6"
+        style={{ backgroundImage: `url(${backgroundUrl})`, backgroundSize: 'cover' }}
+      >
+        <div className="grow"></div> {/* 占位符，讓按鈕在底部 */}
+        <div className="flex justify-center gap-4 items-end pb-4">
+          <Link href="/">
+            <Image
+              src="https://wei-kuo1004.github.io/zhfz2024/images/PRD/back.png"
+              alt="描述文字"
+              width={800}
+              height={600}
+              style={{ objectFit: 'cover', width: '100%', height: 'auto' }}
+            />
+          </Link>
+        </div>
+      </div>
   );
 };
 
