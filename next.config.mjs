0489035fc704f508f04183fe6e4a6ac97f0919fb
weikuo <wei-kuo@pcbut.com.tw>
2024-06-30 19:36:54 +0800

    (feat)bg.png(test)

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
-    unoptimized: true, // 禁用Next.js图片优化，以支持静态导出
+    unoptimized: true, // 禁用Next.js圖片優化，以支持靜態導出
+    // 新增 loader 設定
+    loader: 'akamai',
+    path: '',
   },
   basePath: process.env.NODE_ENV === 'production' ? '/zhfz2024' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/zhfz2024/' : '',
+  // 添加壓縮設置
+  compress: true,
+  // 確保 Webpack 進行代碼分割
+  webpack(config) {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
