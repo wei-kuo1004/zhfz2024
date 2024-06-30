72049f608593c09b1b0d2bbdd9c02b889a1919cc
weikuo <wei-kuo@pcbut.com.tw>
2024-06-30 20:26:37 +0800

    (style)color change

--- components/loadingDot.tsx
+++ components/loadingDot.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const LoadingDots: React.FC = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-yellow-400">
       <div className="flex space-x-2">
-        <div className="h-3 w-3 animate-bounce rounded-full bg-gray-500 delay-0"></div>
-        <div className="h-3 w-3 animate-bounce rounded-full bg-gray-500 delay-200"></div>
-        <div className="delay-400 h-3 w-3 animate-bounce rounded-full bg-gray-500"></div>
+        <div className="h-3 w-3 animate-bounce rounded-full bg-gray-800 delay-0"></div>
+        <div className="h-3 w-3 animate-bounce rounded-full bg-gray-800 delay-200"></div>
+        <div className="delay-400 h-3 w-3 animate-bounce rounded-full bg-gray-800"></div>
       </div>
     </div>
   );
