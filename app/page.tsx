2dae74ce0dfb41f0a0dc2e6a8a6a8f95719db520
weikuo <wei-kuo@pcbut.com.tw>
2024-07-01 08:12:22 +0800

    (fix)loading bug

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,12 @@ export default function IndexPage() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // 從 localStorage 獲取初始值
+     // 每次 clickCount 改變時更新 localStorage
     const savedCount = localStorage.getItem("clickCount");
     if (savedCount) {
       setClickCount(parseInt(savedCount, 10));
     }
 
-    // 等待所有圖片加載完成
     const images = [
       "https://wei-kuo1004.github.io/zhfz2024/images/PRD/p1-bg.png",
       "https://wei-kuo1004.github.io/zhfz2024/images/PRD/p1-start.png",
@@ -27,15 +26,15 @@ export default function IndexPage() {
   }, []);
 
   useEffect(() => {
-    // 每次 clickCount 改變時更新 localStorage
     localStorage.setItem("clickCount", clickCount.toString());
   }, [clickCount]);
 
   const loadImage = (src: string) => {
-    return new Promise<void>((resolve) => {
+    return new Promise<void>((resolve, reject) => {
       const img = new window.Image();
       img.src = src;
       img.onload = () => resolve();
+      img.onerror = () => reject();
     });
   };
 
@@ -44,24 +43,10 @@ export default function IndexPage() {
   };
 
   if (isLoading) {
-    return <LoadingDots/>;
+    return <LoadingDots />;
   }
 
   return (
-    <>
-      <Head>
-        {/* 預加載背景圖片 */}
-        <link
-          rel="preload"
-          href="https://wei-kuo1004.github.io/zhfz2024/images/PRD/p1-bg.png"
-          as="image"
-        />
-        <link
-          rel="preload"
-          href="https://wei-kuo1004.github.io/zhfz2024/images/PRD/p1-start.png"
-          as="image"
-        />
-      </Head>
       <div className="container mx-auto flex aspect-[1/1.8] min-h-screen flex-col items-center justify-center bg-p1-bg bg-cover p-6">
         <div className="w-full flex-1 p-8">
           {/* 你的內容 */}
@@ -76,6 +61,7 @@ export default function IndexPage() {
               alt="描述文字"
               width={800}
               height={600}
+              priority
               style={{ objectFit: "cover", width: "100%", height: "auto" }}
             />
           </Link>
@@ -83,6 +69,5 @@ export default function IndexPage() {
         {/* 顯示本日日期及遊玩人次 */}
         <ClickCounter clickCount={clickCount} />
       </div>
-    </>
   );
 }
